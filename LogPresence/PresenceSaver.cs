12301bef6a9c4fdbdd5175ade3b20ca0ca663dd7
bjornm <bjornm@nhn.no>
2014-01-21 12:37:43 +0100

    Initial commit

---
+++ LogPresence/PresenceSaver.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text.RegularExpressions;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace LogPrescense
+{
+    public class PresenceSaver : ServiceBase
+    {
+        public PresenceSaver()
+        {
+            CanHandleSessionChangeEvent = true;
+            ServiceName = "LogPresence";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log("Service started");
+        }
+
+        protected override void OnStop()
+        {
+            Log("Service stopping");
+            base.OnStop();
+        }
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            Log("Event: {0} Session ID: {1}", changeDescription.Reason.ToString(), changeDescription.SessionId);
+            base.OnSessionChange(changeDescription);
+        }
+
+        public static void Log(string s, params object[] objs)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(@"C:\temp\Presence.txt", true))
+                {
+                    sw.WriteLine(string.Format("{0}: {1}", DateTime.Now.ToString("dd.MM.yyyy HH:mm"), string.Format(s, objs)));
+                }
+                PostProcessIfNewDay();
+            }
+            catch(Exception ex)
+            {
+                try
+                {
+                    File.WriteAllText(@"c:\temp\Presence_Error.txt", "Error: " + ex);
+                }
+                catch{}
+            }
+        }
+
+        private static DateTime _lastProcessDate;
+
+        public static void PostProcessIfNewDay()
+        {
+            if (_lastProcessDate == DateTime.Today)
+                return;
+
+            PostProcess();
+
+            _lastProcessDate = DateTime.Today;
+        }
+
+        private static void PostProcess()
+        {
+            var parsedLogData = ParseLogData(@"C:\temp\Presence.txt");
+
+            var fi = new FileInfo(@"c:\temp\PresenceHours.xlsx");
+
+            if (fi.Exists)
+                File.Delete(fi.FullName);
+
+            using (var f = new StreamWriter(@"C:\temp\PresenceHours.csv", false))
+            using (var xl = new ExcelPackage(fi))
+            {
+                var ws = xl.Workbook.Worksheets.Add("The Hours");
+
+                ws.Cells["A1"].Value = "Dato";
+                ws.Cells["B1"].Value = "Totalt";
+                ws.Cells["C1"].Value = "Norm";
+                ws.Cells["D1"].Value = "Ov50";
+                ws.Cells["E1"].Value = "Ov100";
+                ws.Cells["F1"].Value = "Tid inn";
+                ws.Cells["G1"].Value = "Tid ut";
+
+                ws.Column(1).Width = 19;
+
+                ws.Cells["A1:G1"].Style.Font.Bold = true;
+
+                int rowNo = 2;
+
+                foreach (var logEntry in parsedLogData)
+                {
+                    var diff = logEntry.LeaveTime - logEntry.EnterTime;
+                    var totalHours = (decimal)diff.TotalMinutes / 60m;
+                    var normHours = Math.Min(7.5m, totalHours);
+                    var pcs50Hours = Math.Min((13m - 7.5m), totalHours - normHours);
+                    var pcs100Hours = totalHours - (normHours + pcs50Hours);
+
+                    if (totalHours != (normHours + pcs50Hours + pcs100Hours))
+                        throw new InvalidOperationException("programmer idiot");
+
+                    ws.Cells[rowNo, 1].Value = logEntry.Date;
+                    ws.Cells[rowNo, 2].Value = totalHours;
+                    ws.Cells[rowNo, 3].Value = normHours;
+                    ws.Cells[rowNo, 4].Value = pcs50Hours;
+                    ws.Cells[rowNo, 5].Value = pcs100Hours;
+                    ws.Cells[rowNo, 6].Value = logEntry.EnterTime;
+                    ws.Cells[rowNo, 7].Value = logEntry.LeaveTime;
+
+
+                    if (logEntry.Date.DayOfWeek == DayOfWeek.Monday)
+                    {
+                        ws.Row(rowNo).Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        ws.Row(rowNo).Style.Fill.BackgroundColor.SetColor(Color.FromArgb(200, 255, 200));
+                    }
+
+                    f.WriteLine("{0};{1:0.00};{2:0.00};{3:0.00};{4:0.00};{5};{6}", logEntry.Date.ToString("yyyy-MM-dd"), totalHours, normHours, pcs50Hours, pcs100Hours, logEntry.EnterTime.ToString("hh\\:mm"), logEntry.LeaveTime.ToString("hh\\:mm"));
+
+                    rowNo++;
+                    //Console.WriteLine("{0};{1:0.00};{2:0.00};{3:0.00};{4:0.00}", logEntry.Date.ToString("yyyy-MM-dd"), totalHours, normHours, pcs50Hours, pcs100Hours);
+                }
+                ws.Cells["A2:A" + rowNo].Style.Numberformat.Format = "yyyy-mm-dd";
+                ws.Cells["B2:E" + rowNo].Style.Numberformat.Format = "0.00";
+                ws.Cells["F2:G" + rowNo].Style.Numberformat.Format = "hh:mm";
+                ws.View.FreezePanes(2, 1);
+
+                xl.Save();
+            }
+        }
+
+        private static List<LogEntry> ParseLogData(string path)
+        {
+            var logEntries = new List<LogEntry>();
+            var current = new LogEntry();
+            var state = EventType.Out;
+
+            foreach (var line in File.ReadLines(path))
+            {
+                try {
+                    var comp = line.Split(new[] { ": " }, 2, StringSplitOptions.None);
+                    var time = DateTime.ParseExact(comp[0], "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
+                    var eventType = GetEventType(comp[1]);
+
+                    if (current.Date != time.Date)
+                    {
+                        //Switched date, close previous
+                        if (current.Date != default(DateTime))
+                        {
+                            if (current.Date.AddDays(1) == time.Date && state == EventType.In && eventType == EventType.Out) //Worked over midnight, probably.
+                            {
+                                current.LeaveTime = TimeSpan.FromHours(24);
+                                logEntries.Add(current);
+                                current = new LogEntry {EnterTime = time.TimeOfDay, Date = time.Date};
+                            }
+                            else
+                            {
+                                if (state != EventType.Out)
+                                    Console.WriteLine("Warning, no end to {0}..", current.Date);
+                                else
+                                    logEntries.Add(current);
+                            }
+                        }
+
+                        current = new LogEntry
+                        {
+                            EnterTime = time.TimeOfDay,
+                            Date = time.Date
+                        };
+                        state = EventType.In;
+                    }
+                    else
+                    {
+                        state = eventType;
+                        current.LeaveTime = time.TimeOfDay;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Eh, line {0} failed with {1}", line, ex.Message);
+                }
+            }
+
+            return logEntries;
+        }
+
+        private enum EventType
+        {
+            In, Out
+        }
+
+        private static readonly string[] EventsIn = { "SessionUnlock", "Service started", "SessionLogon", "ConsoleConnect", "RemoteConnect" };
+        private static readonly string[] EventsOut = { "SessionLock", "SessionLogoff", "ConsoleDisconnect", "RemoteDisconnect", "Service stopping" };
+        private static EventType GetEventType(string logData)
+        {
+            if (EventsIn.Any(logData.Contains))
+                return EventType.In;
+            if (EventsOut.Any(logData.Contains))
+                return EventType.Out;
+
+            throw new InvalidOperationException("Unknown event type " + logData);
+        }
+
+        public class LogEntry
+        {
+            public DateTime Date;
+            public TimeSpan EnterTime;
+            public TimeSpan LeaveTime;
+        }
+    }
+}
---
+++ LogPresence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.ServiceProcess;
+
+namespace LogPrescense
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            if (Environment.UserInteractive)
+            {
+                PresenceSaver.PostProcessIfNewDay();
+            }
+            else
+            {
+                var servicesToRun = new ServiceBase[]
+                {
+                    new PresenceSaver()
+                };
+                ServiceBase.Run(servicesToRun);
+            }
+        }
+    }
+}
